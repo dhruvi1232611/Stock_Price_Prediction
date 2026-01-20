0f1cbbd7177ac8cc1150d532d2f701ddc78f799f
Dhruvi Patel <dhruvi2611patel@gmail.com>
2026-01-20 12:00:01 +0530

    model change done

--- stock_predict.py
+++ stock_predict.py
@@ -2,26 +2,23 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import joblib as jb
 from statsmodels.tsa.stattools import adfuller
-#from statsmodels.tsa.arima.model import ARIMA
 import warnings
 warnings.filterwarnings('ignore')
-from sklearn.metrics import root_mean_squared_error,r2_score
+from sklearn.metrics import root_mean_squared_error
 from pmdarima import auto_arima
 import yfinance as yf
+import numpy as np
 
+company = "AAPL"   # Apple
+ticker = yf.Ticker(company)
 
-companies = ["AAPL"]
+# Last 2 years daily data
+df = ticker.history(period="2y")
 
-all_data = []
+df.reset_index(inplace=True)
+df['Company'] = company
 
-for c in companies:
-    df = yf.Ticker(c).history(period="2y")
-    df.reset_index(inplace=True)
-    df["Company"] = c
-    all_data.append(df)
-
-final_df = pd.concat(all_data)
-final_df.to_csv("data/stocks_last_2_years.csv", index=False)
+df.to_csv("data/stocks_last_2_years.csv", index=False)
 
 
 df=pd.read_csv("data/stocks_last_2_years.csv")
@@ -29,15 +26,13 @@ print(df.head())
 print(f"\nMissing Value Count:\n{df.isnull().sum()}")
 
 
-company = "AAPL"
-df = df[df["Company"] == company]
-
 print(f"Statistics:{df.describe()}")
 df['Date']=pd.to_datetime(df['Date'])
 df.set_index("Date", inplace=True)
 df = df.sort_index()
 
 ts=df['Close'].dropna()
+ts_log = np.log(ts)
 
 
 def adf_test(timeseries):
@@ -46,25 +41,22 @@ def adf_test(timeseries):
     print(f"Statistics:{result[0]}")
     print(f"p-value:{result[1]}")
 
-adf_test(ts)
+adf_test(ts_log)
 
-train_size=int(len(ts)*0.8)
+train_size=int(len(ts_log)*0.8)
 train,test=ts[:train_size],ts[train_size:]
 
-#model=ARIMA(train,order=(5,1,0))
-#model_fit=model.fit()
 
 model=auto_arima(
-    train,seasonal=False,
+    train,
+    seasonal=True,
+    m=1,
     trace=True,
     error_action="ignore",
     suppress_warnings=True,
-    start_p=1,
-    start_Q=1,
-    max_p=5,
-    max_q=5,
     trend='t',
-    stepwise=True
+    stepwise=False,
+    d=None,
 )
 
 
@@ -72,7 +64,8 @@ forecast=model.predict(n_periods=len(test))
 rmse=root_mean_squared_error(test,forecast)
 print(f"root_mean_squared_error Score:{rmse}")
 
-
+plt.plot(ts)
+plt.show()
 
 plt.plot(train,label='Train')
 plt.plot(test,label='Test')
